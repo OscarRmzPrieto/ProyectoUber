3b9565ca6a04232bdb8c3aaa26812db4e8374bc5
RubsRz <rodriguezperezrubenalejandro@gmail.com>
2021-11-14 19:02:50 -0600

    cambio

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/proyectoUber/index.html'));
 });
+//Nuevo cambio
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
